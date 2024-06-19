6e53010c874912a8aca13980c0e9ce019024e1b4
gabang2 <offbeat1020@naver.com>
2024-06-20 07:30:02 +0900

    Improvement : 감성 추출 시간 줄임

--- app/routers/keyword_router.py
+++ app/routers/keyword_router.py
@@ -1,5 +1,7 @@
 # Fast Api
-from fastapi import APIRouter
+import time
+
+from fastapi import APIRouter, Depends
 
 # App
 from app.schemas.keyword_schema import KeywordResponse
@@ -11,12 +13,19 @@ router = APIRouter(
     tags=["Keyword API"],
 )
 
+# Create a single instance of KeywordService
 keyword_service = KeywordService()
+
+
+# Dependency injection function
+def get_keyword_service():
+    return keyword_service
 
 
 # Main Section
 @router.get("", response_model=KeywordResponse)
-async def get_keyword(goods_code: str = None):
+async def get_keyword(goods_code: str = None, keyword_service: KeywordService = Depends(get_keyword_service)):
+
     evaluation_text = keyword_service.get_evaluations(goods_code)
     topic, sentiment = keyword_service.united_Processor(evaluation_text)
 
